98c44e30393e963f6afc283d28a453797aa0593e
Michael Palmer <mpalmer1661@gmail.com>
2026-03-25 05:34:56 -0500

    init project

---
+++ src/main/kotlin/dkt/dispatch/DispatchAccountsKtApplication.kt
@@ -0,0 +1,11 @@
+package dkt.dispatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DispatchAccountsKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<DispatchAccountsKtApplication>(*args)
+}
